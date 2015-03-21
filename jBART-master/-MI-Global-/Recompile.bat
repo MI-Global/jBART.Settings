19ce6afd9251b17ea6a6a9562535a6aa05d45d56
Mamaiker <doc@sochi.com>
2015-03-21 23:27:14 +0400

    fix

--- jBART-master/-MI-Global-/Recompile.bat
+++ jBART-master/-MI-Global-/Recompile.bat
@@ -6,18 +6,18 @@ set HomeDir=%CD%
 rmdir /S /Q %HomeDir%\_temp
 
 mkdir %HomeDir%\_temp
-copy /Y data\settings\repo.miui.v4.*.conf %HomeDir%\_temp\
+copy /Y data\settings\repo.miui.v6.*.conf %HomeDir%\_temp\
 echo Copying temporary files...
  
 fnr --cl --dir "%HomeDir%\data\settings" --fileMask "repo.*.conf" --useRegEx --find "folder::[\S]*_local_repo" --replace "folder:://%HomeDir%\_local_repo" >nul
 fnr --cl --dir "%HomeDir%\_temp" --fileMask "repo.*.conf" --useRegEx --find "#[ \S]*[\n]*" --replace "" >nul
 fnr --cl --dir "%HomeDir%\_temp" --fileMask "repo.*.conf" --useRegEx --find "=[ \S]*[\n]*" --replace " " >nul
 
-set /p RepoMain=<%HomeDir%\_temp\repo.miui.v4.main.conf
+set /p RepoMain=<%HomeDir%\_temp\repo.miui.v6.main.conf
 set RepoMain=%RepoMain:~0,-1%
-set /p RepoPatch=<%HomeDir%\_temp\repo.miui.v4.patch.conf
+set /p RepoPatch=<%HomeDir%\_temp\repo.miui.v6.patch.conf
 set RepoPatch=%RepoPatch:~0,-1%
-set /p RepoExtra=<%HomeDir%\_temp\repo.miui.v4.extra.conf
+set /p RepoExtra=<%HomeDir%\_temp\repo.miui.v6.extra.conf
 set RepoExtra=%RepoExtra:~0,-1%
 
 del /Q /F %HomeDir%\_temp\repo.*.conf
