199617f0307b4485b2e6992cfa23c7a652b5e4ff
Bormental <sexis@mail.ru>
2015-07-10 19:52:42 +0300

    add 7zip archiver and fix deodex.bat

--- jBART-master/_input_miui/delete_odex_files.bat
+++ jBART-master/_input_miui/delete_odex_files.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%1"=="" goto usage
-..\7z d %1 *.odex -r -y 
+7za d %1 *.odex -r -y 
 goto end
 
 :usage
@@ -8,7 +8,7 @@ echo.
 echo            How to use this file:
 echo.
 echo.
-echo Type deodex.bat [path][input_miui_ROM].zip
+echo Type delete_odex_files.bat [path][input_miui_ROM].zip
 echo.
 echo.
 echo.
